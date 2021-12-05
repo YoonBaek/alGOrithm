83da298b0dc66b404b56557f316fa38055a0a263
YoonBaek <royal729@naver.com>
2021-12-05 09:35:49 +0900

    code: Solve BOJ 01753, 1629

---
+++ BOJ/mathematics/01629/01629-곱셈_yoonbaek.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+var a, b, c int
+
+func fastPow(x, y int) int {
+	if y == 1 {
+		return x % c
+	}
+	fp := fastPow(x, y/2) % c
+	if y%2 == 0 {
+		return fp * fp % c
+	}
+	return fp * fp % c * x % c
+}
+
+func main() {
+	fmt.Scanf("%d %d %d\n", &a, &b, &c)
+	fmt.Println(fastPow(a, b))
+}
